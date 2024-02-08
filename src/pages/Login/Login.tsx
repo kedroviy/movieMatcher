37659c3e405222cf070b4b72f09511403fe01eb1
Aliaksei <36371745+kedroviy@users.noreply.github.com>
2024-02-09 01:19:48 +0300

    feat: add google auth

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,10 @@
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native"
+import { View, Text, StyleSheet } from "react-native";
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    User,
+} from '@react-native-google-signin/google-signin';
+import { useEffect, useState } from "react";
 
 const styles = StyleSheet.create({
     container: {
@@ -14,14 +20,75 @@ const styles = StyleSheet.create({
     },
 });
 
+
 export const LoginScreen = () => {
-    const onPress = () => console.log('button work');
+    const [user, setUser] = useState<User | undefined>();
+
+    useEffect(() => {
+        GoogleSignin.configure({
+            webClientId: '277043180680-qdj0s8aiqvf24rpj411ertlp3liv4gj7.apps.googleusercontent.com',
+            offlineAccess: true,
+            forceCodeForRefreshToken: true,
+        });
+        isSignedIn();
+        console.log('user: ', user)
+    }, []);
+
+    const signIn = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo: User = await GoogleSignin.signIn();
+            console.log('__due__ :', userInfo)
+            setUser(userInfo)
+
+        } catch (error) {
+            if (error) {
+                console.log(error)
+            }
+        }
+    };
+
+    const isSignedIn = async () => {
+        const isSignedIn = await GoogleSignin.isSignedIn();
+        if (!!isSignedIn) {
+            getCurrentUserInfo()
+        } else {
+            console.log('please login')
+        }
+    };
+
+    const getCurrentUserInfo = async () => {
+        try {
+            const userInfo: User = await GoogleSignin.signInSilently();
+            console.log('currentUserInfo: ', userInfo)
+
+            setUser(userInfo);
+        } catch (error) {
+            if (error) {
+                console.log('Error message: ', error)
+            }
+        }
+    };
+
+    const signOut = async () => {
+        try {
+            await GoogleSignin.signOut();
+            setUser({ user: null } as unknown as User);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <View style={styles.container}>
             <Text>Login page</Text>
-            <TouchableOpacity style={styles.button} onPress={onPress}>
-                <Text>Press Here</Text>
-            </TouchableOpacity>
+
+            <GoogleSigninButton
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Dark}
+                onPress={signIn}
+            />
+
         </View>
     )
 }
